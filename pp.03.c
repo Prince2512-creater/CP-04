3a9f6421c5baf5909fcf31e4105f11b2b2adb27f
Prince2512-creater <rkgoc1010@gmail.com>
2025-09-08 15:14:27 +0530

    Add files via upload

---
+++ pp.03.c
@@ -0,0 +1,31 @@
+int main()
+{
+    int a[5];
+    int i,j, temp;
+
+    printf("enter five integer values");
+    for(i=0;i<5;i++)
+    scanf("%d",&a[i]);
+
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+
+    printf("array sorted in ascending order:\n");
+    for(i=0;i<5;i++)
+    {
+        printf("%d",a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
